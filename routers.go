901b393598e96d270b87074eee99aa7fc718b4ca
Dmitry <avhimkov@mail.ru>
2018-01-21 20:35:01 +0500

    refactoring routers. Add permission bolt.

--- routers.go
+++ routers.go
@@ -1,53 +1,81 @@
 package main
 
+import (
+	"fmt"
+	"strings"
+	"net/http"
+	"log"
+
+	"github.com/gin-gonic/gin"
+	"github.com/xyproto/permissionbolt"
+)
+
 func initializeRoutes() {
 
-	// Use the setUserStatus middleware for every route to set a flag
-	// indicating whether the request was from an authenticated user or not
-	// router.Use(setUserStatus())
+	g := gin.New()
+	perm, err := permissionbolt.New()
+	if err != nil {
+		log.Fatalln(err)
+	}
 
-	// Handle the index route
-	router.GET("/", showIndexPage)
+	userstate := perm.UserState()
 
-	// Group user related routes together
-	// userRoutes := router.Group("/u")
-	// {
-	// 	// Handle the GET requests at /u/login
-	// 	// Show the login page
-	// 	// Ensure that the user is not logged in by using the middleware
-	// 	userRoutes.GET("/login", ensureNotLoggedIn(), showLoginPage)
+	g.GET("/", func(c *gin.Context) {
+		msg := ""
+		msg += fmt.Sprintf("Has user bob: %v\n", userstate.HasUser("bob"))
+		msg += fmt.Sprintf("Logged in on server: %v\n", userstate.IsLoggedIn("bob"))
+		msg += fmt.Sprintf("Is confirmed: %v\n", userstate.IsConfirmed("bob"))
+		msg += fmt.Sprintf("Username stored in cookies (or blank): %v\n", userstate.Username(c.Request))
+		msg += fmt.Sprintf("Current user is logged in, has a valid cookie and *user rights*: %v\n", userstate.UserRights(c.Request))
+		msg += fmt.Sprintf("Current user is logged in, has a valid cookie and *admin rights*: %v\n", userstate.AdminRights(c.Request))
+		msg += fmt.Sprintln("\nTry: /register, /confirm, /remove, /login, /logout, /makeadmin, /clear, /data and /admin")
+		c.String(http.StatusOK, msg)
+	})
 
-	// 	// Handle POST requests at /u/login
-	// 	// Ensure that the user is not logged in by using the middleware
-	// 	userRoutes.POST("/login", ensureNotLoggedIn(), performLogin)
+	g.GET("/register", func(c *gin.Context) {
+		userstate.AddUser("bob", "hunter1", "bob@zombo.com")
+		c.String(http.StatusOK, fmt.Sprintf("User bob was created: %v\n", userstate.HasUser("bob")))
+	})
 
-	// 	// Handle GET requests at /u/logout
-	// 	// Ensure that the user is logged in by using the middleware
-	// 	userRoutes.GET("/logout", ensureLoggedIn(), logout)
+	g.GET("/confirm", func(c *gin.Context) {
+		userstate.MarkConfirmed("bob")
+		c.String(http.StatusOK, fmt.Sprintf("User bob was confirmed: %v\n", userstate.IsConfirmed("bob")))
+	})
 
-	// 	// Handle the GET requests at /u/register
-	// 	// Show the registration page
-	// 	// Ensure that the user is not logged in by using the middleware
-	// 	userRoutes.GET("/register", ensureNotLoggedIn(), showRegistrationPage)
+	g.GET("/remove", func(c *gin.Context) {
+		userstate.RemoveUser("bob")
+		c.String(http.StatusOK, fmt.Sprintf("User bob was removed: %v\n", !userstate.HasUser("bob")))
+	})
 
-	// 	// Handle POST requests at /u/register
-	// 	// Ensure that the user is not logged in by using the middleware
-	// 	userRoutes.POST("/register", ensureNotLoggedIn(), register)
-	// }
+	g.GET("/login", func(c *gin.Context) {
+		// Headers will be written, for storing a cookie
+		userstate.Login(c.Writer, "bob")
+		c.String(http.StatusOK, fmt.Sprintf("bob is now logged in: %v\n", userstate.IsLoggedIn("bob")))
+	})
 
-	// // Group article related routes together
-	// articleRoutes := router.Group("/article")
-	// {
-	// 	// Handle GET requests at /article/view/some_article_id
-	// 	articleRoutes.GET("/view/:article_id", getArticle)
+	g.GET("/logout", func(c *gin.Context) {
+		userstate.Logout("bob")
+		c.String(http.StatusOK, fmt.Sprintf("bob is now logged out: %v\n", !userstate.IsLoggedIn("bob")))
+	})
 
-	// 	// Handle the GET requests at /article/create
-	// 	// Show the article creation page
-	// 	// Ensure that the user is logged in by using the middleware
-	// 	articleRoutes.GET("/create", ensureLoggedIn(), showArticleCreationPage)
+	g.GET("/makeadmin", func(c *gin.Context) {
+		userstate.SetAdminStatus("bob")
+		c.String(http.StatusOK, fmt.Sprintf("bob is now administrator: %v\n", userstate.IsAdmin("bob")))
+	})
 
-	// 	// Handle POST requests at /article/create
-	// 	// Ensure that the user is logged in by using the middleware
-	// 	articleRoutes.POST("/create", ensureLoggedIn(), createArticle)
-	// }
+	g.GET("/clear", func(c *gin.Context) {
+		userstate.ClearCookie(c.Writer)
+		c.String(http.StatusOK, "Clearing cookie")
+	})
+
+	g.GET("/data", func(c *gin.Context) {
+		c.String(http.StatusOK, "user page that only logged in users must see!")
+	})
+
+	g.GET("/admin", func(c *gin.Context) {
+		c.String(http.StatusOK, "super secret information that only logged in administrators must see!\n\n")
+		if usernames, err := userstate.AllUsernames(); err == nil {
+			c.String(http.StatusOK, "list of all users: "+strings.Join(usernames, ", "))
+		}
+	})
 }
