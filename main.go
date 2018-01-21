0dd52f1bf6eae5814280cf45130d6d6a4e156b04
Dmitry <avhimkov@mail.ru>
2018-01-21 20:57:28 +0500

    refactoring main.go and delete routers

--- main.go
+++ main.go
@@ -1,19 +1,20 @@
 package main
 
 import (
-	"net/http"
-	"log"
 	"fmt"
+	"log"
+	"net/http"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 	"github.com/xyproto/permissionbolt"
 )
 
-var router *gin.Engine
+//var router *gin.Engine
 
 func main() {
 	// Set Gin to production mode
-	gin.SetMode(gin.ReleaseMode)
+	//gin.SetMode(gin.ReleaseMode)
 
 	// Set the router as the default one provided by Gin
 	//router = gin.Default()
@@ -24,6 +25,10 @@ func main() {
 		log.Fatalln(err)
 	}
 
+	// Blank slate, no default permissions
+	//perm.Clear()
+
+	// Set up a middleware handler for Gin, with a custom "permission denied" message.
 	permissionHandler := func(c *gin.Context) {
 		// Check if the user has the right admin/user rights
 		if perm.Rejected(c.Writer, c.Request) {
@@ -47,28 +52,72 @@ func main() {
 
 	// Process the templates at the start so that they don't have to be loaded
 	// from the disk again. This makes serving HTML pages very fast.
-	 router.LoadHTMLGlob("templates/*")
+	// router.LoadHTMLGlob("templates/*")
 
 	// Initialize the routes
-	initializeRoutes()
+	//initializeRoutes()
 
+	// Get the userstate, used in the handlers below
+	userstate := perm.UserState()
+
+	g.GET("/", func(c *gin.Context) {
+		msg := ""
+		msg += fmt.Sprintf("Has user bob: %v\n", userstate.HasUser("bob"))
+		msg += fmt.Sprintf("Logged in on server: %v\n", userstate.IsLoggedIn("bob"))
+		msg += fmt.Sprintf("Is confirmed: %v\n", userstate.IsConfirmed("bob"))
+		msg += fmt.Sprintf("Username stored in cookies (or blank): %v\n", userstate.Username(c.Request))
+		msg += fmt.Sprintf("Current user is logged in, has a valid cookie and *user rights*: %v\n", userstate.UserRights(c.Request))
+		msg += fmt.Sprintf("Current user is logged in, has a valid cookie and *admin rights*: %v\n", userstate.AdminRights(c.Request))
+		msg += fmt.Sprintln("\nTry: /register, /confirm, /remove, /login, /logout, /makeadmin, /clear, /data and /admin")
+		c.String(http.StatusOK, msg)
+	})
+
+	g.GET("/register", func(c *gin.Context) {
+		userstate.AddUser("bob", "hunter1", "bob@zombo.com")
+		c.String(http.StatusOK, fmt.Sprintf("User bob was created: %v\n", userstate.HasUser("bob")))
+	})
+
+	g.GET("/confirm", func(c *gin.Context) {
+		userstate.MarkConfirmed("bob")
+		c.String(http.StatusOK, fmt.Sprintf("User bob was confirmed: %v\n", userstate.IsConfirmed("bob")))
+	})
+
+	g.GET("/remove", func(c *gin.Context) {
+		userstate.RemoveUser("bob")
+		c.String(http.StatusOK, fmt.Sprintf("User bob was removed: %v\n", !userstate.HasUser("bob")))
+	})
+
+	g.GET("/login", func(c *gin.Context) {
+		// Headers will be written, for storing a cookie
+		userstate.Login(c.Writer, "bob")
+		c.String(http.StatusOK, fmt.Sprintf("bob is now logged in: %v\n", userstate.IsLoggedIn("bob")))
+	})
+
+	g.GET("/logout", func(c *gin.Context) {
+		userstate.Logout("bob")
+		c.String(http.StatusOK, fmt.Sprintf("bob is now logged out: %v\n", !userstate.IsLoggedIn("bob")))
+	})
+
+	g.GET("/makeadmin", func(c *gin.Context) {
+		userstate.SetAdminStatus("bob")
+		c.String(http.StatusOK, fmt.Sprintf("bob is now administrator: %v\n", userstate.IsAdmin("bob")))
+	})
+
+	g.GET("/clear", func(c *gin.Context) {
+		userstate.ClearCookie(c.Writer)
+		c.String(http.StatusOK, "Clearing cookie")
+	})
+
+	g.GET("/data", func(c *gin.Context) {
+		c.String(http.StatusOK, "user page that only logged in users must see!")
+	})
+
+	g.GET("/admin", func(c *gin.Context) {
+		c.String(http.StatusOK, "super secret information that only logged in administrators must see!\n\n")
+		if usernames, err := userstate.AllUsernames(); err == nil {
+			c.String(http.StatusOK, "list of all users: "+strings.Join(usernames, ", "))
+		}
+	})
 	// Start serving the application
-	router.Run()
+	g.Run(":3000")
 }
-
-//func render(c *gin.Context, data gin.H, templateName string) {
-//	loggedInInterface, _ := c.Get("is_logged_in")
-//	data["is_logged_in"] = loggedInInterface.(bool)
-//
-//	switch c.Request.Header.Get("Accept") {
-//	case "application/json":
-//		// Respond with JSON
-//		c.JSON(http.StatusOK, data["payload"])
-//	case "application/xml":
-//		// Respond with XML
-//		c.XML(http.StatusOK, data["payload"])
-//	default:
-//		// Respond with HTML
-//		c.HTML(http.StatusOK, templateName, data)
-//	}
-//}
