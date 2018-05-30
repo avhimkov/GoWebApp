21f428e8fb7e80ee0d6339491f3a03978858adf7
Dmitry <avhimkov@mail.ru>
2018-05-30 23:49:10 +0500

    refactoring main.go

--- main.go
+++ main.go
@@ -394,9 +394,26 @@ func main() {
 	g.Run(":3000")
 }
 
-func Edit(c *gin.Context) {
+// func Logger() gin.HandlerFunc {
+// 	return func(c *gin.Context) {
+// 		t := time.Now()
 
-}
+// 		// Set example variable
+// 		c.Set("example", "12345")
+
+// 		// before request
+
+// 		c.Next()
+
+// 		// after request
+// 		latency := time.Since(t)
+// 		log.Print(latency)
+
+// 		// access the status we are sending
+// 		status := c.Writer.Status()
+// 		log.Println(status)
+// 	}
+// }
 
 func EditValue(c *gin.Context) {
 	uid := c.Param("uid")
