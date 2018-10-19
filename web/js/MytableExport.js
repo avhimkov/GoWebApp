cdcef793ade8331e7b9cabf950f84828f7426a22
Dmitry <avhimkov@mail.ru>
2018-10-19 11:42:16 +0500

    multi table fix

--- web/js/MytableExport.js
+++ web/js/MytableExport.js
@@ -7,3 +7,23 @@ $(document).ready(function() {
     table.buttons().container()
         .appendTo( '#table_wrapper .col-md-6:eq(0)' );
 } );
+
+$(document).ready(function() {
+    var table1 = $('#table1').DataTable( {
+        lengthChange: false,
+        buttons: [ 'copy', 'excel', 'pdf', 'colvis' ]
+    } );
+ 
+    table1.buttons().container()
+        .appendTo( '#table1_wrapper .col-md-6:eq(0)' );
+} );
+
+$(document).ready(function() {
+    var table2 = $('#table2').DataTable( {
+        lengthChange: false,
+        buttons: [ 'copy', 'excel', 'pdf', 'colvis' ]
+    } );
+ 
+    table2.buttons().container()
+        .appendTo( '#table2_wrapper .col-md-6:eq(0)' );
+} );
