74e6fdaf2a14c1973d0852f06b12a50dc0b5409d
Dmitry <avhimkov@mail.ru>
2018-10-23 00:02:05 +0500

    work right menu

--- web/js/MytableExport.js
+++ web/js/MytableExport.js
@@ -50,7 +50,7 @@ window.onscroll = function() {
   if (prevScrollpos > currentScrollPos) {
     document.getElementById("navbar").style.top = "0";
   } else {
-    document.getElementById("navbar").style.top = "-70px";
+    document.getElementById("navbar").style.top = "-90px";
   }
   prevScrollpos = currentScrollPos;
 }
