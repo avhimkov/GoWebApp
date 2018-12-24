0368a67a6bd95c6f0f48c493adaab3cc2637f62c
Dmitry <avhimkov@mail.ru>
2018-12-24 21:30:06 +0500

    fix scroll table and menu

--- web/js/My.js
+++ web/js/My.js
@@ -1,5 +1,20 @@
+/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos < currentScrollPos) {
+    document.getElementById("navbar").style.top = "-90px";
+  } else {
+    document.getElementById("navbar").style.top = "0";
+  }
+  prevScrollpos = currentScrollPos;
+}
+
 $(document).ready(function() {
-    var table = $('#table').DataTable( {
+    var table = $('#table').DataTable( 
+        {
+        "scrollX":true,
+        "scroller":false,
         lengthChange: false,
         buttons: [ {extend: 'copy',  filename: 'Data export'}, 
                    {extend: 'csv',  filename: 'Data export'}, 
@@ -13,8 +28,20 @@ $(document).ready(function() {
         .appendTo( '#table_wrapper .col-md-6:eq(0)' );
 } );
 
+// $(document).ready( function () {
+//     var table = $('#table').DataTable(
+//       {
+//           "scrollY":400,
+//           "scrollX":true,
+//           "scroller":false
+//       }
+//     );
+//   } );
+
 $(document).ready(function() {
     var table1 = $('#table1').DataTable( {
+        "scrollX":true,
+        "scroller":false,
         lengthChange: false,
         buttons: [ {extend: 'copy',  filename: 'Data export'}, 
                    {extend: 'csv',  filename: 'Data export'}, 
@@ -30,6 +57,8 @@ $(document).ready(function() {
 
 $(document).ready(function() {
     var table2 = $('#table2').DataTable( {
+        "scrollX":true,
+        "scroller":false,
         lengthChange: false,
         buttons: [ {extend: 'copy',  filename: 'Data export'}, 
                    {extend: 'csv',  filename: 'Data export'}, 
@@ -43,14 +72,3 @@ $(document).ready(function() {
         .appendTo( '#table2_wrapper .col-md-6:eq(0)' );
 } );
 
-/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
-var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
-  var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementById("navbar").style.top = "0";
-  } else {
-    document.getElementById("navbar").style.top = "-90px";
-  }
-  prevScrollpos = currentScrollPos;
-}
