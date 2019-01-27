1ac3ad78f3463b710122a53fcf940d48fb6af70b
Dmitry <avhimkov@mail.ru>
2019-01-27 19:18:09 +0500

    commit mongo code

--- main.go
+++ main.go
@@ -6,7 +6,6 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
-	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
@@ -18,11 +17,11 @@ import (
 	"github.com/asdine/storm/q"
 	"github.com/gin-gonic/gin"
 	"github.com/jung-kurt/gofpdf"
-	"github.com/mongodb/mongo-go-driver/mongo"
+
+	// "github.com/mongodb/mongo-go-driver/mongo"
 	"github.com/thinkerou/favicon"
 	"github.com/xyproto/permissionbolt"
-	"github.com/zebresel-com/mongodm"
-	mgo "gopkg.in/mgo.v2"
+	// "github.com/zebresel-com/mongodm"
 	// "github.com/prometheus/client_golang/prometheus"
 	// "github.com/prometheus/client_golang/prometheus/promauto"
 	// "github.com/prometheus/client_golang/prometheus/promhttp"
@@ -71,7 +70,7 @@ type Person struct {
 }
 
 //
-// var db = DB()
+var db = DB()
 
 //middlleware db
 var perm, err = perminit("db/bolt.db")
@@ -81,7 +80,7 @@ var userstate = perm.UserState()
 var permissionHandler = permHandler()
 
 //open databas
-/* func DB() *storm.DB {
+func DB() *storm.DB {
 	//data db
 	db, err := storm.Open("db/data.db")
 	if err != nil {
@@ -89,37 +88,37 @@ var permissionHandler = permHandler()
 	}
 	// defer db.Close()
 	return db
-} */
-
-func mogoDBinit() {
-	file, err := ioutil.ReadFile("locals.json")
-
-	if err != nil {
-		fmt.Printf("File error: %v\n", err)
-		os.Exit(1)
-	}
-
-	var localMap map[string]map[string]string
-	json.Unmarshal(file, &localMap)
-
-	dbConfig := &mongodm.Config{
-		DialInfo: &mgo.DialInfo{
-			Addrs:    []string{"127.0.0.1"},
-			Timeout:  3 * time.Second,
-			Database: "mongodm_sample",
-			Username: "admin",
-			Password: "admin",
-			Source:   "admin",
-		},
-		Locals: localMap["en-US"],
-	}
-
-	connection, err := mongodm.Connect(dbConfig)
-
-	if err != nil {
-		fmt.Println("Database connection error: %v", err)
-	}
 }
+
+// func mogoDBinit() {
+// 	file, err := ioutil.ReadFile("locals.json")
+
+// 	if err != nil {
+// 		fmt.Printf("File error: %v\n", err)
+// 		os.Exit(1)
+// 	}
+
+// 	var localMap map[string]map[string]string
+// 	json.Unmarshal(file, &localMap)
+
+// 	dbConfig := &mongodm.Config{
+// 		DialInfo: &mgo.DialInfo{
+// 			Addrs:    []string{"127.0.0.1"},
+// 			Timeout:  3 * time.Second,
+// 			Database: "mongodm_sample",
+// 			Username: "admin",
+// 			Password: "admin",
+// 			Source:   "admin",
+// 		},
+// 		Locals: localMap["en-US"],
+// 	}
+
+// 	connection, err := mongodm.Connect(dbConfig)
+
+// 	if err != nil {
+// 		fmt.Println("Database connection error: %v", err)
+// 	}
+// }
 
 //middlleware init
 func perminit(db string) (*permissionbolt.Permissions, error) {
@@ -190,7 +189,7 @@ func SetupRouter() *gin.Engine {
 
 func main() {
 
-	client, err := mongo.NewClient("mongodb://localhost:27017")
+	// client, err := mongo.NewClient("mongodb://localhost:27017")
 
 	//prometheus
 	// http.Handle("/metrics", promhttp.Handler())
